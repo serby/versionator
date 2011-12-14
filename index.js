3731101b0ec5866e21f4894e107e253ad828438d
Paul Serby <paul.serby@clock.co.uk>
2011-12-14 11:45:24 +0000

    Initial version

---
+++ index.js
@@ -0,0 +1,32 @@
+var
+	url = require('url'),
+	path = require('path');
+
+module.exports = function(version) {
+	return function(req, res, next) {
+		if (req.method !== 'GET') {
+			return next();
+		}
+
+		// Ensure version is a string
+		version = '' + version;
+
+		var
+			urlParts = url.parse(req.url),
+			basename = path.basename(urlParts.pathname),
+			dirname = path.dirname(urlParts.pathname),
+			vPos = dirname.length - version.length;
+
+
+		// If version isn't in path then move on.
+		if (dirname.substring(vPos, vPos + version.length) !== version) {
+			return next();
+		}
+
+		// Rebuild the URL without the version and set the request url.
+		urlParts.pathname = path.join(dirname.substring(0, vPos), basename);
+		req.url = url.format(urlParts);
+
+		next();
+	};
+};
