84bb1edf54275ba1003cdcb77f5cb4f3dba7162d
Paul Serby <paul.serby@clock.co.uk>
2012-01-29 09:33:06 +0000

    Adding basic connect example

---
+++ examples/basic/server.js
@@ -0,0 +1,13 @@
+var 
+	version = '0.1',
+	connect = require('connect'),
+	versionator = require('../../');
+
+connect(
+	versionator('v' + version),
+	connect.static(__dirname + '/public', { maxAge: 0 }),
+	function(req, res) {
+		res.setHeader('Content-Type', 'text/html');
+		res.end('<script src="/js/v' + version + '/example.js"></script>');
+	}
+).listen(3000);
