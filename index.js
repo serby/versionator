7e70750621c17a69483ebaa2b82321e5b5ab3833
Paul Serby <paul.serby@clock.co.uk>
2012-01-30 22:42:11 +0000

    Adding map path helper

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 module.exports.createBasicMiddleware = require('./lib/basic-middleware');
 module.exports.createMappedMiddleware = require('./lib/mapped-middleware');
+module.exports.createMapFromPath = require('./lib/map-path');
---
+++ test/map-path.test.js
@@ -0,0 +1,81 @@
+var
+	assert = require('assert'),
+	request = require('request'),
+	versionator = require('../'),
+	appEngine = require('connect'),
+	fs = require('fs'),
+	async = require('async');
+
+function createFiles(dirPath, files, callback) {
+	var fns = [
+		async.apply(fs.mkdir, dirPath),
+		async.apply(fs.mkdir, dirPath + '/sub')
+	];
+	
+	Object.keys(files).forEach(function(filename) {
+		fns.push(async.apply(fs.writeFile, dirPath + '/' + filename, files[filename]));
+	});
+
+	async.series(fns, callback);
+}
+
+function removeFiles(dirPath, files, callback) {
+	var fns = [];
+	
+	Object.keys(files).forEach(function(filename) {
+		fns.push(async.apply(fs.unlink, dirPath + '/' + filename));
+	});
+
+	fns.push(async.apply(fs.rmdir, dirPath + '/sub'));
+	fns.push(async.apply(fs.rmdir, dirPath));
+
+	async.series(fns, callback);
+}
+
+describe('versionator', function() {
+
+	var
+		tmpPath = '/tmp/versionator-test',
+		files = {
+			'a': '',
+			'b': 'Hello',
+			'c': 'World!',
+			'sub/a': 'hi'
+		};
+
+	before(function(done) {
+		createFiles(tmpPath, files, function() {
+			done();
+		});
+	});
+
+	describe('map path validation', function() {
+
+		it('should error if an invalid path is given', function() {
+
+			versionator.createMapFromPath('', function(error) {
+				error.should.be.instanceof(Error).with.property('message').eql('Path is required');
+			});
+		});
+
+		it('should correctly walk directory and create hashes', function(done) {
+
+			versionator.createMapFromPath(tmpPath, function(error, results) {
+				
+				var a = {
+					'/a': 'd41d8cd98f00b204e9800998ecf8427e', 
+					'/b': '8b1a9953c4611296a827abf8c47804d7',
+					'/c': 'e509465ef513154988e088d6ad3c21bf',
+					'/sub/a': '49f68a5c8493ec2c0bf489821c21fc3b' }.should.eql(results);
+
+				done();
+			});
+
+		});
+
+	});
+
+	after(function(done) {
+		removeFiles(tmpPath, files, done);
+	});
+});
